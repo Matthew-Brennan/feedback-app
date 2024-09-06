34d64613ce65f44b267529440cf99354c21ab37b
Matthew-Brennan <matthew.brennan4@hotmail.com>
2024-09-05 20:04:20 -0400

    lots o props

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,22 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Text 1'
+
+    },
+    {
+        id: 2,
+        rating: 5,
+        text: 'Text 2'
+
+    },
+    {
+        id: 3,
+        rating: 0,
+        text: 'Text 3'
+
+    }
+]
+
+export default FeedbackData
